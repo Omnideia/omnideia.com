8c9874f65580709f1dd824f3ea1d71293eef4341
Junior Alves <jjalves96@gmail.com>
2020-11-20 09:48:15 -0300

    chore: add new props on Global Style

--- styles/global.ts
+++ styles/global.ts
@@ -6,4 +6,16 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     box-sizing: border-box;
   }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  html, body, #__next {
+    height: 100%;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
 `;
