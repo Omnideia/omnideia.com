db89da8905701d01a2a911956f3196b30e3a3828
Junior Alves <jjalves96@gmail.com>
2020-11-20 10:04:53 -0300

    chore: add new props on GlobalStyles

--- styles/global.ts
+++ styles/global.ts
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
-export const GlobalStyle = createGlobalStyle`
+const GlobalStyles = createGlobalStyle`
   * {
     padding: 0;
     margin: 0;
@@ -19,3 +19,5 @@ export const GlobalStyle = createGlobalStyle`
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
   }
 `;
+
+export default GlobalStyles;
