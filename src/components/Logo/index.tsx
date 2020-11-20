2c2049f8cce61dac12a9f6023b50263f4d0efb8b
Junior Alves <jjalves96@gmail.com>
2020-11-20 16:06:11 -0300

    config: remove tag Image API from Nextjs

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -5,7 +5,7 @@ import * as S from './styles';
 const Logo: React.FC = () => {
   return (
     <S.Container>
-      <S.Logo src="/assets/logo.png" alt="Logo" width={500} height={500} />
+      <S.Logo src="/assets/logo.png" alt="Logo" />
     </S.Container>
   );
 };
