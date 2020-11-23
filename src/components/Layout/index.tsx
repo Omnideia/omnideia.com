fcb439062032bc25eba86756581d84cdf08686da
Junior Alves <jjalves96@gmail.com>
2020-11-23 16:18:31 -0300

    chore: add Header component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 
-// import Header from '../Header';
+import Header from '../Header';
 
 import * as S from './styles';
 
 const Layout: React.FC = ({ children }) => {
   return (
     <S.Container>
-      {/* <Header /> */}
-      <S.Main>
-        <S.Text>In Progress...</S.Text>
-        <S.LogoContainer>
-          <S.Logo src="assets/logoExpand.png" />
-        </S.LogoContainer>
-        {children}
-      </S.Main>
+      <Header />
+      <S.Main>{children}</S.Main>
     </S.Container>
   );
 };
